f93c4aec2f79a62e4e276dde557b32bba3bf516b
Thaypann <cleitonfelipesn@gmail.com>
2025-08-01 21:05:15 +0000

    Adicionado noticia - segunda imagem apresentação

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index ():
-    return render_template('index.html')
+    noticias = [
+        {"titulo": "Notícia 1", "descricao": "Resumo sobre tecnologia", "imagem": "noticia1.jpg", "link": "https://exemplo.com/noticia1"},
+        {"titulo": "Notícia 2", "descricao": "Resumo sobre educação", "imagem": "noticia2.jpg", "link": "https://exemplo.com/noticia2"},
+        {"titulo": "Notícia 3", "descricao": "Resumo sobre meio ambiente", "imagem": "noticia3.jpg", "link": "https://exemplo.com/noticia3"},
+        {"titulo": "Notícia 4", "descricao": "Resumo sobre ciência", "imagem": "noticia4.jpg", "link": "https://exemplo.com/noticia4"},
+        {"titulo": "Notícia 5", "descricao": "Resumo sobre esportes", "imagem": "noticia5.jpg", "link": "https://exemplo.com/noticia5"},
+        {"titulo": "Notícia 6", "descricao": "Resumo sobre cultura", "imagem": "noticia6.jpg", "link": "https://exemplo.com/noticia6"}
+    ]
+    return render_template("index.html", noticias=noticias)
 
 @app.route('/professor')
 def professor ():
